17365efd20aa9b249ce934140e43db486a4958cb
lz <kavilee@163.com>
2019-11-04 23:00:20 +0800

    1104

--- PosalServer/goods/models.py
+++ PosalServer/goods/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
 
-class GoodsCategory(models.Model):
-    
+class Category(models.Model):
+    code=models.CharField(verbose_name='编号')
 
 
 
 class Goods(models.Model):
+    barcode=models.CharField(verbose_name='条码')
 
