bbb7a432a6d767bd01a29455c03a9cf0332f56d1
lz <kavilee@163.com>
2019-11-04 18:41:38 +0800

    init

--- PosalServer/goods/models.py
+++ PosalServer/goods/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class GoodsCategory(models.Model):
+    
+
+
+
+class Goods(models.Model):
+
--- PosalServer/user/models.py
+++ PosalServer/user/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class UserProfile(AbstractUser): 
+    mobile=models.CharField(verbose_name='电话')
+
+
