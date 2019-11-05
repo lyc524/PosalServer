5585c47a4a9e4854935ecbe40790fce7e5e69baa
lz <kavilee@163.com>
2019-11-05 23:12:09 +0800

    xxx

--- PosalServer/goods/models.py
+++ PosalServer/goods/models.py
@@ -5,9 +5,16 @@ from django.db import models
 
 class Category(models.Model):
     code=models.CharField(verbose_name='编号')
+    name=models.CharField(verbose_name='类别名称')
+    parent_id=models.CharField(verbose_name='父编号')
 
 
 
 class Goods(models.Model):
+    category=models.ForeignKey(Category,on_delete=models.CASCADE,verbose_name='类别ID')
     barcode=models.CharField(verbose_name='条码')
+    goods_name=models.CharField(verbose_name='商品名称')
+    purchase_price=models.FloatField("进价",default=0)
+    sale_price=models.FloatField("售价",default=0)
+    profit=models.FloatField("利润",default=0)
 
--- PosalServer/goods/views.py
+++ PosalServer/goods/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet 
+from .models import Category,Goods
+from .serializer import CategorySerializer,GoodsSerializer
 
 # Create your views here.
 
 class CategoryViewSet(ModelViewSet): 
+    queryset=Category.objects.all()
+    serializer_class=CategorySerializer
 
+    
+class GoodsViewSet(ModelViewSet):
+    queryset=Goods.objects.all()
+    serializer_class=GoodsSerializer
