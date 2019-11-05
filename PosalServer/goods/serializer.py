5233823e9f63f7b82378fa296176bd9e8edcfdc4
lz <kavilee@163.com>
2019-11-05 18:02:54 +0800

    init

--- PosalServer/goods/serializer.py
+++ PosalServer/goods/serializer.py
@@ -6,3 +6,8 @@ class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model=Category
         fields="__all__"
+
+class GoodsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Goods
+        fields="__all__"
